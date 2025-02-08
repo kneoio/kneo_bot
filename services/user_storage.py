9b2075b5d4adfcab601994c3aba6a5a742edc88e
kaira <justaidajam@gmail.com>
2025-02-08 22:30:10 +0000

    #

--- services/user_storage.py
+++ services/user_storage.py
@@ -1,21 +1,19 @@
 from dotenv import load_dotenv
 from google.cloud.firestore_v1.base_query import FieldFilter
-from services.firebase_client import FirebaseClient
 from models.Member import Member
 from utils.logger import logger
 
 load_dotenv()
 
+
 class UserStorageClient:
     def __init__(self):
-        self.db = FirebaseClient.get_db()
+        print("")
 
     def check_user(self, telegram_name: str) -> Member:
         try:
             docs = (
-                self.db.collection("members")
-                .where(filter=FieldFilter("telegramName", "==", telegram_name))
-                .stream()
+
             )
 
             member = None
@@ -36,8 +34,7 @@ class UserStorageClient:
     def register_user(self, telegram_name: str) -> Member:
         try:
             member = Member(telegram_name)
-            doc_ref = self.db.collection("members").document(telegram_name)
-            doc_ref.set(member.__dict__)
+
             return member
         except Exception as e:
             logger.error(f"Error registering user: {e}")
